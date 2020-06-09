79765ef69ca61b520ad747b0b2435fddd0f86d9e
Renguin <maityrahul7@gmail.com>
2020-06-09 22:43:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function setup() {
-    frameRate(30);
-    textSize(30);
-    textAlign(CENTER);
+function setup(){
+	createCanvas(1000, 600);
 }
 
-function draw() {
-    background(200);
-    text(frameCount, width / 2, height / 2);
+function draw(){
+	background(220);
+	rect(30, 20, 55, 55);
+	rect(90, 20, 55, 55);
+	rect(150, 20, 55, 55);
+	rect(240, 20, 55, 55);
+	rect(480, 20, 55, 55);
 }
