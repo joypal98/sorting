1dab2b14718134ab0a6ae28b3bd910ba64dd4721
Renguin <maityrahul7@gmail.com>
2020-06-10 00:08:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function setup(){
 	createCanvas(1000, 600);
+		background(220);
 }
 
-function draw(){
-	background(220);
-}
 
 let sortArr = [];
 
 if($("button").click(function(){
 	let sortString = ($("input").val());
     sortArr = sortString.split(',');
-     function draw(){
-	let X = 30;
-      for(let i = 0; i < sortArr.length ; i++){
-      	console.log(sortArr[i]);
-   	  	rect(X, 20, parseInt(sortArr[i]) , 55);
-   	  	X = X + 55 + 5;
-   }
-}
+ 
     } 
   )
 );
+
+    function draw(){
+
+	let X = 30;
+      for(let i = 0; i < sortArr.length ; i++){
+      	console.log(sortArr[i]);
+   	  	rect(X, 20, 55 , parseInt(sortArr[i]));
+   	  	X = X + 55 + 5;
+   }
+   }
