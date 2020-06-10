309d35deb93ae27dc9d61110ea61fb887bdd1251
Renguin <maityrahul7@gmail.com>
2020-06-11 02:45:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
 function setup(){
-	createCanvas(1000, 600);
-		background(220);
+  createCanvas(1000, 600);
+    background(220);
 }
 
 
-let sortArr = [];
+let sortArr = new Array(30);
+let numArr = new Array(30);
 
 if($("button").click(function(){
-	let sortString = ($("input").val());
+  let sortString = ($("input").val());
     sortArr = sortString.split(',');
- 
+    for(let i = 0 ; i < sortArr.length ; i++){
+       numArr[i] = parseInt(sortArr[i]);
+       console.log(numArr[i])
+    }
     } 
   )
 );
 
-    function draw(){
+let i=0;
+ let j=0;
+ let temp=0;
+  let p=0;
+$(".start").click(function{
+  function draw()
 
-	let X = 30;
-      for(let i = 0; i < sortArr.length ; i++){
-      	console.log(sortArr[i]);
-   	  	rect(X, 20, 55 , parseInt(sortArr[i]));
-   	  	X = X + 55 + 5;
-   }
-   }
+   {
+    console.log(i+"   "+j); 
+    console.log(numArr);
+    background(0);
+
+    
+
+
+      if(numArr[j]>numArr[j+1])
+      {
+
+        temp=numArr[j];
+        numArr[j]=numArr[j+1];
+        numArr[j+1]=temp;
+      }
+          
+      
+    
+      if(i<numArr.length)
+        {
+          j=j+1;
+          if (j>=numArr.length-i-1)
+            {
+              j=0;
+              i=i+1;
+            }
+        }
+       
+       if(i===35)
+       {
+        noLoop();
+       }
+    
+  for(let k=0;k<numArr.length;k++)
+    {   
+      fill(250,0,200);
+      
+      rect(p,10,10,numArr[k]);
+      p=p+10;
+    }
+    p=0;
+  console.log(i+"   "+j); 
+  }
+}
+  )
+   
