188f1b3f309b31cd7cb7d9ef8a212f911af6b5de
joypal98 <64988924+joypal98@users.noreply.github.com>
2020-06-16 22:00:20 +0530

    Update sort2.js

--- sort2.js
+++ sort2.js
@@ -1,8 +1,9 @@
-let i=0;
+ let i=0;
  let j=0;
  let temp=0;
  let p=0;
  let flag=0;
+ let w=0;
 
 let values = [33,190,80,7,340,100,78,55,121,222,44,111,98];
 let rr;
@@ -11,8 +12,9 @@ console.log(values);
   function setup()
   {
     createCanvas(800,600);
-    frameRate(5);
+    frameRate(20);
     let button=createButton("reset");
+    slider=createSlider(3,50,10,2);
     button.mousePressed(change);
     background(33);
    
@@ -27,6 +29,8 @@ console.log(values);
  function draw()
    {  
     background(0);
+    translate(0,height);
+    w=slider.value();
     if(i>values.length)
          {
              noLoop();
@@ -43,8 +47,8 @@ console.log(values);
        for(let k=0;k<values.length;k++)
        {
         fill(200,0,200);
-        rect(p,10,20,values[k]);
-        p=p+20;
+        rect(p,10,w,-values[k]);
+        p=p+w;
        }
        p=0; 
        }                            
@@ -66,6 +70,77 @@ function st()
 
     }
   }
+
+
+//***********************************************************************************************************
+// let i=0;
+//  let j=0;
+//  let temp=0;
+//  let p=0;
+//  let flag=0;
+
+// let values = [33,190,80,7,340,100,78,55,121,222,44,111,98];
+// let rr;
+// console.log(values);
+
+//   function setup()
+//   {
+//     createCanvas(800,600);
+//     frameRate(5);
+//     let button=createButton("reset");
+//     button.mousePressed(change);
+//     background(33);
+   
+//   }
+  
+
+//   function change()
+//   {
+//     flag=1;
+//   }
+
+//  function draw()
+//    {  
+//     background(0);
+//     if(i>values.length)
+//          {
+//              noLoop();
+//            }
+//        if(values[j]>values[j+1])
+//          {
+                                      
+//              let temp=values[j];
+//              values[j]=values[j+1];
+//              values[j+1]=temp;  
+//          }
+//        st();
+                                   
+//        for(let k=0;k<values.length;k++)
+//        {
+//         fill(200,0,200);
+//         rect(p,10,20,values[k]);
+//         p=p+20;
+//        }
+//        p=0; 
+//        }                            
+// function st()
+//   {
+//     if(flag===1)
+//     {
+
+//       if(i<values.length)
+//         {
+//           j=j+1;
+//           if (j>=values.length-i-1)
+//             {
+//               j=0;
+//               i=i+1;}
+//               console.log(i+","+j);
+                                    
+//             }
+
+//     }
+//   }
    
 
 //***************************
